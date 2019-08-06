1dfb5cf11c419270ec26e2933c635310fa31b636
Stanislav Kozlov <bleymy@mail.ru>
2019-08-06 20:28:27 +0300

    Initial Commit

---
+++ Climatic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Climatic
+//
+//  Created by Stanislav on 06/08/2019.
+//  Copyright © 2019 Stanislav Kozlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
